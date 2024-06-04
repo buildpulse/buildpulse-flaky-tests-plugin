b65c7234731072e025c5cb0ac2941e930d746495
Sid Dange <dange.siddhant@gmail.com>
2024-06-04 14:29:48 -0400

    finalized plugin

---
+++ README.md
@@ -0,0 +1,110 @@
+
+# Jenkins for BuildPulse [![Jenkins license](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
+[BuildPulse](https://buildpulse.io/products/flaky-tests) is a CI observability and optimization platform that helps you find and fix flaky tests instantly, as well as help cut test times in half.
+
+This Jenkins plugin works with our free tier (+ others), and allows you to easily connect your Jenkins CI workflows to BuildPulse.
+
+## Usage
+
+1. Locate the BuildPulse credentials for your account at [buildpulse.io][]
+2. In your Jenkins settings, create a [username and password credential](https://www.jenkins.io/doc/book/using/using-credentials/) with the ID:`buildpulse-credentials`, username:`BUILDPULSE_ACCESS_KEY_ID` and password:`BUILDPULSE_SECRET_ACCESS_KEY` (values can be found in the BuildPulse Dashboard)
+3. Populate the `BUILDPULSE_ACCOUNT_ID` and `BUILDPULSE_REPOSITORY_ID` in your Jenkinsfile (values can be found in the BuildPulse Dashboard)
+4. Add a step to your Jenkins workflow to use this plugin to send your test results to BuildPulse:
+```
+post {
+  always {
+    withCredentials([usernamePassword(credentialsId: 'buildpulse-credentials', usernameVariable: 'BUILDPULSE_ACCESS_KEY_ID', passwordVariable: 'BUILDPULSE_SECRET_ACCESS_KEY')]) {
+      buildpulseStep accountId: env.BUILDPULSE_ACCOUNT_ID, repositoryId: env.BUILDPULSE_REPOSITORY_ID, path: "/spec/reports", key: env.BUILDPULSE_ACCESS_KEY_ID, secret: env.BUILDPULSE_SECRET_ACCESS_KEY, commit: env.GIT_COMMIT, branch: env.GIT_BRANCH
+    }
+  }
+}
+```
+## Inputs
+
+### `account`
+
+**Required** The unique numeric identifier for the BuildPulse account that owns the repository.
+
+### `repository`
+
+**Required** The unique numeric identifier for the repository being built.
+
+### `path`
+
+**Required** The path to the XML file(s) for the test results. Can be a directory (e.g., `test/reports`), a single file (e.g., `reports/junit.xml`), or a glob (e.g., `app/*/results/*.xml`).
+
+### `keyCredentialId`
+
+**Required** The Jenkins Credential (Username and Password) ID. For the credential, set the username to the value of `BUILDPULSE_ACCESS_KEY_ID` in the dashboard. Set the password to the value of `BUILDPULSE_SECRET_ACCESS_KEY` in the dashboard.
+
+### `commit`
+
+**Required** The SHA for the commit that produced the test results.
+
+If your workflow checks out a _different_ commit than the commit that triggered the workflow, then use this input to specify the commit SHA that your workflow checked out. For example, if your workflow is triggered by the `pull_request`, but you customize the workflow to check out the pull request merge commit, then you'll want to set this input to the pull request HEAD commit SHA.
+
+### `branch`
+
+**Required** The branch of the current build.
+
+### `repositoryPath`
+
+_Optional_ The path to the local git clone of the repository (default: workspace directory).
+
+### `coveragePaths`
+
+_Optional_ The paths to the coverage file(s) for the test results (space-separated).
+
+### `tags`
+
+_Optional_ Tags to apply to this build (space-separated).
+
+### `quota`
+
+_Optional_ Quota ID to count this upload against. Please set on BuildPulse Dashboard first.
+
+## Example Jenkinsfile
+```
+    pipeline {
+    agent {
+        docker {
+            image 'ruby:3.0.5' // Specify the Ruby container image you want to use
+            args '-u root' // Optionally, specify additional Docker container run arguments
+        }
+    }
+
+
+    environment {
+        GIT_REPO_URL = 'https://github.com/BuildPulseLLC/decentralized-forums' // Replace with your repository URL
+        GIT_BRANCH = 'main' // Replace with your repository branch
+        BUILDPULSE_ACCOUNT_ID = '' // Replace with your BuildPulse account ID
+        BUILDPULSE_REPOSITORY_ID = '' // Replace with your BuildPulse repository ID
+    }
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                script {
+                    echo 'Cloning the repository...'
+                    scmVars = git branch: "${env.GIT_BRANCH}", url: "${env.GIT_REPO_URL}", credentialsId: 'github'
+                    env.GIT_COMMIT = scmVars.GIT_COMMIT
+                }
+            }
+        }
+
+        stage('Setup & Test') {
+            // install dependencies & run tests
+        }
+    }
+
+    post {
+        always {
+            withCredentials([usernamePassword(credentialsId: 'buildpulse-credentials', usernameVariable: 'BUILDPULSE_ACCESS_KEY_ID', passwordVariable: 'BUILDPULSE_SECRET_ACCESS_KEY')]) {
+              buildpulseStep accountId: env.BUILDPULSE_ACCOUNT_ID, repositoryId: env.BUILDPULSE_REPOSITORY_ID, junitXMLPaths: "/spec/reports", key: env.BUILDPULSE_ACCESS_KEY_ID, secret: env.BUILDPULSE_SECRET_ACCESS_KEY, commitSHA: env.GIT_COMMIT, branch: env.GIT_BRANCH
+            }
+        }
+    }
+}
+```
+
+[buildpulse.io]: https://buildpulse.io
---
+++ src/main/java/io/jenkins/plugins/buildpulse/BuildPulseCommand.java
@@ -0,0 +1,201 @@
+package io.jenkins.plugins.buildpulse;
+
+import hudson.FilePath;
+import hudson.FilePath.FileCallable;
+import hudson.remoting.VirtualChannel;
+import jenkins.security.Roles;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.jenkinsci.remoting.RoleChecker;
+import org.kohsuke.stapler.verb.POST;
+
+public class BuildPulseCommand {
+    private String accountId;
+    private String repositoryId;
+    private String junitXmlReportPaths;
+    private String keyId;
+    private String secret;
+    private FilePath workspacePath;
+
+    // optional arguments
+    private String coveragePaths;
+    private String tags;
+    private String quota;
+    private FilePath repositoryPath;
+
+    public static String[] submit(String accountId, String repositoryId, String junitXmlReportPaths, String repositoryPath, String keyId, String secret, FilePath workspacePath, String coveragePaths, String tags, String quota) throws IOException, InterruptedException {
+        BuildPulseCommand command = new BuildPulseCommand(accountId, repositoryId, junitXmlReportPaths, repositoryPath, keyId, secret, workspacePath, coveragePaths, tags, quota);
+        return command.downloadAndGetCommand();
+    }
+
+    public BuildPulseCommand(String accountId, String repositoryId, String junitXmlReportPaths, String repositoryPath,
+            String keyId, String secret, FilePath workspacePath, String coveragePaths, String tags, String quota) {
+        this.accountId = accountId;
+        this.repositoryId = repositoryId;
+        this.junitXmlReportPaths = junitXmlReportPaths;
+        this.keyId = keyId;
+        this.secret = secret;
+        this.workspacePath = workspacePath;
+
+        // optional arguments
+        this.repositoryPath = repositoryPath != null && repositoryPath.length() > 0 ? new FilePath(workspacePath, repositoryPath) : workspacePath;
+
+        if (coveragePaths != null) {
+            this.coveragePaths = coveragePaths;
+        }
+
+        if (tags != null) {
+            this.tags = tags;
+        }
+
+        if (quota != null) {
+            this.quota = quota;
+        }
+    }
+
+    @POST
+    @SuppressWarnings("lgtm[jenkins/no-permission-check]")
+    public String[] downloadAndGetCommand() throws IOException, InterruptedException {
+        this.validateArguments();
+
+        ArrayList<String> validatedJunitXmlReportPaths = this.resolveFilePaths(this.junitXmlReportPaths);
+        ArrayList<String> validatedCoverageReportPaths = this.resolveFilePaths(this.coveragePaths);
+
+        FilePath binaryFilePath = this.downloadBuildPulseBinary();
+
+        ArrayList<String> command = new ArrayList<String>(Arrays.asList(binaryFilePath.getRemote(), "submit"));
+        command.addAll(validatedJunitXmlReportPaths);
+        command.addAll(Arrays.asList("--account-id", this.accountId, "--repository-id", this.repositoryId, "--repository-dir", this.repositoryPath.getRemote()));
+
+        if (validatedCoverageReportPaths.size() > 0) {
+            command.add("--coverage-files");
+            command.addAll(validatedCoverageReportPaths);
+        }
+
+        if (this.tags != null && this.tags.length() > 0) {
+            command.add("--tags");
+            command.add(this.tags);
+        }
+
+        if (this.quota != null && this.quota.length() > 0) {
+            command.add("--quota-id");
+            command.add(this.quota);
+        }
+
+        command.add("--disable-coverage-auto");
+
+        return command.toArray(new String[0]);
+    }
+
+    private FilePath downloadBuildPulseBinary() throws IOException, InterruptedException {
+        String binaryFileName = determineBinaryFileName();
+        URL binaryUrl = new URL("https://github.com/buildpulse/test-reporter/releases/latest/download/" + binaryFileName);
+
+        // Use act to safely perform the download on the agent where the workspace resides
+        return workspacePath.act(new FileDownloader(binaryUrl, binaryFileName));
+    }
+
+    private static class FileDownloader implements FileCallable<FilePath>, Serializable {
+        private final URL url;
+        private final String fileName;
+
+        public FileDownloader(URL url, String fileName) {
+            this.url = url;
+            this.fileName = fileName;
+        }
+
+        @Override
+        public void checkRoles(RoleChecker checker) throws SecurityException {
+            checker.check(this, Roles.SLAVE);
+        }
+
+        @Override
+        public FilePath invoke(File f, VirtualChannel channel) throws IOException, InterruptedException {
+            File localFile = new File(f, fileName);
+            FilePath filePath = new FilePath(localFile);
+
+            if (!filePath.exists()) {
+                try (InputStream in = url.openStream(); OutputStream out = filePath.write()) {
+                    byte[] buffer = new byte[4096];
+                    int n;
+                    while ((n = in.read(buffer)) != -1) {
+                        out.write(buffer, 0, n);
+                    }
+                }
+            }
+
+            // Set the file to be executable
+            filePath.chmod(0755);
+
+            return filePath;
+        }
+    }
+
+    private void validateArguments() throws RuntimeException, IOException {
+        if (!this.accountId.matches("^[0-9]+$")) {
+            throw new RuntimeException("Account ID " + this.accountId + " doesn't match expected format: '^[0-9]+$'");
+        }
+
+        if (!this.repositoryId.matches("^[0-9]+$")) {
+            throw new RuntimeException("Repository ID " + this.repositoryId + " doesn't match expected format: '^[0-9]+$'");
+        }
+
+        boolean exists = false;
+        boolean isDir = false;
+        try {
+            exists = this.repositoryPath.exists();
+            isDir = this.repositoryPath.isDirectory();
+        } catch (InterruptedException e) {}
+
+        if (!(exists && isDir)) {
+            throw new RuntimeException("Repository path is not a directory: " + this.repositoryPath);
+        }
+
+        if (this.keyId.length() == 0) {
+            throw new RuntimeException("BuildPulse key is required");
+        }
+
+        if (this.secret.length() == 0) {
+            throw new RuntimeException("BuildPulse secret is required");
+        }
+    }
+
+    private String determineBinaryFileName() throws IOException, InterruptedException {
+        String osName = (String)this.workspacePath.toComputer().getSystemProperties().get("os.name");
+        String binaryFileName;
+
+        if (osName.contains("win")) {
+            binaryFileName = "test-reporter-windows-amd64.exe";
+        } else if (osName.contains("mac")) {
+            binaryFileName = "test-reporter-darwin-amd64";
+        } else {
+            binaryFileName = "test-reporter-linux-amd64";
+        }
+
+        return binaryFileName;
+    }
+
+    private ArrayList<String> resolveFilePaths(String pathsString) throws IOException {
+        if (pathsString == null) {
+            return new ArrayList<String>();
+        }
+
+        String[] paths = pathsString.split(" ");
+        ArrayList<String> resolvedPaths = new ArrayList<String>();
+
+        for (int i = 0; i < paths.length; i++) {
+            String path = paths[i];
+            resolvedPaths.add(new File(this.workspacePath.getRemote(), path.toString()).getPath());
+        }
+
+        return resolvedPaths;
+    }
+}
